e5ab6373dff16535c32b6b7bc9e9262c0e096e2e
PP-lab1023 <nicolepan1023@163.com>
2025-03-29 21:38:57 +0900

    load config with viper

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,12 +6,8 @@ import (
 	"os"
 	"testing"
 
+	"github.com/PP-lab1023/Go-bank/util"
 	_ "github.com/lib/pq" // This is a special import, because no function in this package is used. So a _ should be added.
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/Go-bank?sslmode=disable"
 )
 
 var testQueries *Queries
@@ -19,8 +15,13 @@ var testDB *sql.DB
 
 func TestMain(m *testing.M) {
 	var err error 
+
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot connect to db", err)
+	}
 	
-	testDB, err = sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db", err)
 	}
